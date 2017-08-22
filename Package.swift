b73b03dd4d3207961f0690cf61a91a34a9f05db6
Erik Little <nuclear.ace@gmail.com>
2017-08-22 11:00:02 -0400

    Correct spm deps

--- Package.swift
+++ Package.swift
@@ -24,9 +24,9 @@ let package = Package(
     name: "StarscreamSocketIO",
     dependencies: [
         .Package(url: "https://github.com/daltoniam/zlib-spm.git",
-                 majorVersion: 1),
+                 majorVersion: 1, minor: 1),
         .Package(url: "https://github.com/daltoniam/common-crypto-spm",
-                 majorVersion: 0),
+                 majorVersion: 1, minor: 1),
         ],
     exclude: ["Tests", "examples"]
 )
