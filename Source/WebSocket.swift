f7e28f24ae20898da5804079319da52682bb9212
Erik Little <nuclear.ace@gmail.com>
2017-08-29 09:14:28 -0400

    further namespace for testing

--- Source/WebSocket.swift
+++ Source/WebSocket.swift
@@ -21,7 +21,7 @@
 
 import Foundation
 import CoreFoundation
-import SSCommonCrypto
+import SSSCommonCrypto
 
 public let WebsocketDidConnectNotification = "WebsocketDidConnectNotification"
 public let WebsocketDidDisconnectNotification = "WebsocketDidDisconnectNotification"
